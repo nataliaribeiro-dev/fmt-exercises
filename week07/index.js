54685f11ba04a70a02a20fc7ce2f81d96b706435
Natalia Ribeiro <natilgr@hotmail.com>
2024-03-24 12:53:40 -0300

    create a constructor

--- week07/index.js
+++ week07/index.js
@@ -7,4 +7,11 @@ class Product {
   name;
   price;
   quantity;
+
+  //ex02: Crie um construtor para a classe Produto que permita receber os valores base para cada um dos parâmetros.
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
 }
