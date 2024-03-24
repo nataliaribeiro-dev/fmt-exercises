05abd375bfc04a143574da872915d008a6cfd1a6
Natalia Ribeiro <natilgr@hotmail.com>
2024-03-24 15:11:14 -0300

    create replace method

--- week07/index.js
+++ week07/index.js
@@ -14,6 +14,7 @@ class Product {
     this.price = price;
     this.quantity = quantity;
   }
+  //  ex. 03: Crie um método chamado sell que permita vender uma quantidade do produto e atualize o estoque.
 
   sell(quantity_sold) {
     if (quantity_sold > this.quantity) {
@@ -24,8 +25,16 @@ class Product {
     console.log(`Venda de ${quantity_sold} ${this.name} realizada com sucesso!`);
     console.log(`Estoque atual: ${this.quantity}`);
   }
+
+  //ex04: Crie um método chamado Repor() dentro da classe Produto.
+  // Este método deve receber como parâmetro a quantidade reposta e adicionar o montante à quantidade do produto.
+  replace(quantity_replaced) {
+    this.quantity += quantity_replaced;
+    console.log(`Reposição de ${quantity_replaced} ${this.name}s realizada com sucesso!`);
+    console.log(`Estoque atual: ${this.quantity} unidades.`);
+  }
 }
 
 //let product = new Product("Camiseta", 50, 100);
 
-//product.sell(101);
+//product.replace(30);
